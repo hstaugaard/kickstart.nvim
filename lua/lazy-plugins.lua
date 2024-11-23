09482511e2a92dd7c0d026acddabe35a881b5129
Hans Staugaard <hans.staugaard@prevas.dk>
2024-11-23 15:19:49 +0100

    Ported old configuration

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -44,6 +44,14 @@ require('lazy').setup({
 
   require 'kickstart/plugins/treesitter',
 
+  require 'kickstart/plugins/neo-solarized',
+
+  require 'kickstart/plugins/lualine',
+
+  require 'kickstart/plugins/kitty-scrollback',
+
+  require 'kickstart/plugins/flash',
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
