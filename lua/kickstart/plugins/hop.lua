a7adeedb3ef44e2ceb349c22e8f283826b4868af
Hans Staugaard <hans.staugaard@prevas.dk>
2024-11-29 20:55:07 +0100

    Hop config

--- lua/kickstart/plugins/hop.lua
+++ lua/kickstart/plugins/hop.lua
@@ -1,13 +1,21 @@
-return {{
+return {
+  {
     'smoka7/hop.nvim',
-    version = "*",
+    version = '*',
     config = function()
-        local hop = require('hop')
-        hop.setup { keys = 'etovxqpdygfblzhckisuran' }
-        local directions = require('hop.hint').HintDirection
-        vim.keymap.set('n', '<leader>es', function() hop.hint_char1() end, {remap=true})
-        vim.keymap.set('n', '<leader>ew', function() hop.hint_words({ direction = directions.AFTER_CURSOR }) end, {remap=true})
-        vim.keymap.set('n', '<leader>eb', function() hop.hint_words({ direction = directions.BEFORE_CURSOR }) end, {remap=true})
-    end
-}}
+      local hop = require 'hop'
+      hop.setup { keys = 'etovxqpdygfblzhckisuran' }
+      local directions = require('hop.hint').HintDirection
+      vim.keymap.set('n', '<leader>es', function()
+        hop.hint_char1 {}
+      end, { remap = true })
+      vim.keymap.set('n', '<leader>ew', function()
+        hop.hint_words { direction = directions.AFTER_CURSOR }
+      end, { remap = true })
+      vim.keymap.set('n', '<leader>eb', function()
+        hop.hint_words { direction = directions.BEFORE_CURSOR }
+      end, { remap = true })
+    end,
+  },
+}
 -- vim: ts=2 sts=2 sw=2 et
