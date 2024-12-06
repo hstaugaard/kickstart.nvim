edd12bcebd7a38226285afa5592c04d43dc2eac3
Hans Krüger Staugaard <863764+hstaugaard@users.noreply.github.com>
2024-12-06 10:22:25 +0100

    Remapped surround to <C-s>

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -15,7 +15,17 @@ return {
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = '<C-s>a', -- Add surrounding in Normal and Visual modes
+          delete = '<C-s>d', -- Delete surrounding
+          find = '<C-s>f', -- Find surrounding (to the right)
+          find_left = '<C-s>F', -- Find surrounding (to the left)
+          highlight = '<C-s>h', -- Highlight surrounding
+          replace = '<C-s>r', -- Replace surrounding
+          update_n_lines = '<C-s>n', -- Update `n_lines`
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
