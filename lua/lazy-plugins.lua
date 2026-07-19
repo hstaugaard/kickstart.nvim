bba9e551b3b82a52c75ee89c955ce6bef844aed9
Hans Krüger Staugaard <hstaugaard@user.noreply.github.com>
2026-07-19 16:14:51 +0200

    Added multi-cursor plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -46,7 +46,9 @@ require('lazy').setup({
 
   require 'kickstart/plugins/leap',
 
-  require 'kickstart/plugins/chezmoi'
+  require 'kickstart/plugins/chezmoi',
+
+  require 'kickstart/plugins/multicursor',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
